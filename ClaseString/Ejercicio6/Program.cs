18a24910cdf243ec30d2b07ea138e017630acda9
Matias <matias.b.dev@gmail.com>
2022-08-24 22:36:40 -0300

    EJERCICIO6

--- ClaseString/Ejercicio6/Program.cs
+++ ClaseString/Ejercicio6/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Ejercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           string [,] Usuarios = new string [6,2];   //matriz para guardar los usiarios y las paswords.
+
+            Usuarios[0,0 ] = "Juan";
+            Usuarios[1,0 ] = "Jose";
+            Usuarios[2,0 ] = "Andres";
+            Usuarios[3,0 ] = "Ana";
+            Usuarios[4,0 ] = "Silvana";
+            Usuarios[5,0 ] = "Romina";
+           
+            Console.WriteLine(Usuarios[2,0]);
+
+        }
+
+        static void Preguntar(string [,] U)
+        {
+            string Usuario = "";
+        Console.WriteLine("Ingrese un Usuario: ");
+        
+        Usuario = Console.ReadLine();
+
+        }
+    }
+}
