facb6260e46a6e267bb4848da8bfe61355ffd3fb
Matias <matias.b.dev@gmail.com>
2022-08-24 21:30:11 -0300

    Prueba de la clase string metodo trim

---
+++ ClaseString/TrimPrueba/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PruebaTrim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string caracter = "1@";
+            string nombre1 = "Matias 1@";
+            string nombre2 = "1@ Matias";
+
+            Console.Clear();
+
+            Console.WriteLine(nombre2.TrimStart (caracter.ToCharArray()));
+            Console.WriteLine(nombre1.TrimEnd (caracter.ToCharArray()));
+            
+        }
+    }
+}
