eddfe311aa89105eaebf958fc85334ec5782a02d
Matias Bruna <matias.b.dev@gmail.com>
2022-09-02 21:03:54 -0300

    termino el ejercicio 11, uso del StartWITH y el EndsWith

--- ClaseString/Ejercicio11/Ejercicio11/Program.cs
+++ ClaseString/Ejercicio11/Ejercicio11/Program.cs
@@ -10,6 +10,53 @@ namespace Ejercicio11
     {
         static void Main(string[] args)
         {
+            string[] Carreras =  {"Arquitectura, UTN Cordoba",
+                               "Diseno grafico, UTN Santa Fe",
+                               "Diseno industrial, UTN Cordoba",
+                               "Administracion de empresas, UTN Santa Fe",
+                               "Contador, UTN San Francisco",
+                               "Turismo, UTN Buenos Aires",
+                               "Medicina, UTN Cordoba",
+                               "Qu�mica, UTN Mendoza",
+                               "Historia, UTN La Plata",
+                               "Ingenieria electronica, UTN San Francisco",
+                               "Ingenieria mecanica, UTN San Francisco",
+                               "Ingenieria en sistemas, UTN San Francisco"};
+
+
+
+            Buscar(Carreras);   
+        }
+
+        static void Buscar(string[] V)
+        {
+            Console.Clear();
+            string dato = "";
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Ingrese una carrera o una localidad: ");
+                dato = Console.ReadLine();
+                for (int i = 0; i < V.Length; i++)
+                {
+                    if (V[i].StartsWith(dato))
+                    {
+                        Console.WriteLine(V[i]);
+                    }
+                }
+                for (int i = 0; i < V.Length; i++)
+                {
+                    if (V[i].EndsWith(dato))
+                    {
+                        Console.WriteLine(V[i]);
+                    }
+                }
+                Console.ReadKey();
+                        
+            } while (dato != "s");
+            
+
         }
     }
 }
